c2073a26457e7ac819d870ca85fcf7ca138908c9
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2021-12-21 12:32:34 +0600

    scrips.js

---
+++ script.js
@@ -0,0 +1,39 @@
+// console.log("hello world");
+// var firstName = "Vasya";
+// var lastName = "Urmatbekova";
+// var color = "red";
+// var horseName = "Jannat";
+// var car = "Mers Benz";
+// var nameOfApple = "Aport";
+// var nameOfStreet  = "Tynystanova";
+
+// alert("Hello World");
+// alert("Kyrgysztan");
+
+// console.log(firstName);
+// console.log(color);
+// console.log(horseName);
+// console.log(nameOfApple);
+// console.log(nameOfStreet);
+// console.log(car);
+// alert("hello karakol");
+console.log("Hello world");
+
+// declaration
+var firstName;
+var age;
+
+// initialize
+var lastName = "Volkov";
+
+// assigment
+firstName = "Vasya";
+firstName = "Petya";
+console.log(firstName);
+
+// example 1 math
+var potatoes = 105, tomatoes = 35;
+var vegetables = potatoes + tomatoes;
+console.log(vegetables);
+
+var result = vegetables \ 2;
